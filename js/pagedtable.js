8c2c2e88402bb556270e7bb24bdc202806eec9e9
thebioengineer <ellishughes@live.com>
2020-07-17 22:31:17 -0700

    Fix when nrow is not evenly divided into pages and it shows total rows as too long

--- js/pagedtable.js
+++ js/pagedtable.js
@@ -593,6 +593,11 @@ var PagedTable = function (pagedTable, source) {
       var rowStart = me.getRowStart();
       return rowStart + me.rows;
     };
+    
+    me.getVisRowEnd = function() {
+      var rowStart = me.getRowStart();
+      return Math.min(rowStart + me.rows, data.length);
+    }
 
     me.getPaddingRows = function() {
       var rowStart = me.getRowStart();
@@ -1091,7 +1096,7 @@ var PagedTable = function (pagedTable, source) {
 
   var getLabelInfo = function() {
     var pageStart = page.getRowStart();
-    var pageEnd = page.getRowEnd();
+    var pageEnd = page.getVisRowEnd();
     var totalRows = data.length;
 
     var totalRowsLabel = options.rows.total ? options.rows.total : totalRows;
